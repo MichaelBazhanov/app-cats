e7217c97758170adc12a8506bfccb4d72757f1d6
michael bazhanov <michaelbazhanov33@gmail.com>
2022-10-31 20:12:54 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="antialiased font-sans">
+      <header className="bg-blue">
+        <nav className="flex max-w-7xl mx-auto px-6">
+          <div className="py-6 px-5.5 text-sm cursor-pointer whitespace-nowrap tracking-wide text-white bg-black bg-opacity-10">
+            Все котики
+          </div>
+          <div className="py-6 px-5.5 text-sm cursor-pointer whitespace-nowrap tracking-wide text-white/70 hover:text-white hover:bg-black hover:bg-opacity-10 transition-colors">
+            Любимые котики
+          </div>
+        </nav>
       </header>
+      <main>
+        <h1 className="sr-only">Page Cats</h1>
+
+        {/* this component */}
+        <section>
+          <div className="max-w-7xl mx-auto  px-6 mt-12">Все котики</div>
+        </section>
+
+        {/* this component */}
+        <section>
+          <div className="max-w-7xl mx-auto  px-6 mt-12">Любимые котики</div>
+        </section>
+      </main>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        blue: "#2196F3",
+      },
+      maxWidth: {
+        'max-w-screen-me': '1220%',
+      },
+      spacing: {
+        '5.5': '1.375rem',
+      }
+    },
   },
   plugins: [],
 };
