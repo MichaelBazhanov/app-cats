bfbb779f5e8100acf39f82ca35c97bff7c362cd1
michael bazhanov <michaelbazhanov33@gmail.com>
2022-11-01 18:57:27 +0300

    fix Cats

--- src/components/Cats/index.js
+++ src/components/Cats/index.js
@@ -1,7 +1,7 @@
 import Cat from "../Cat";
 
 const Cats = ({ cats }) => {
-  return <>{cats && cats.map(({ id, url }) => <Cat key={id} id={id} url={url} />)}</>;
+  return <>{cats && cats.map(({ id, url }, idx) => <Cat key={`${idx}${id}`} id={id} url={url} />)}</>;
 };
 
 export default Cats;
