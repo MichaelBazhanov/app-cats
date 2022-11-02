91c8298dad291f501ca5ab18a394f39e35f318db
michael bazhanov <michaelbazhanov33@gmail.com>
2022-11-02 19:57:36 +0300

    Added Action to Remove Favourite Cats

--- src/modules/catFavourite/actions.js
+++ src/modules/catFavourite/actions.js
@@ -1,3 +1,4 @@
+// SET
 export const SET_CAT_FAVOURITE = "SET_CAT_FAVOURITE";
 export const CAT_FAVOURITE_SUCCESS = "CAT_FAVOURITE_SUCCESS";
 export const CAT_FAVOURITE_FAILURE = "CAT_FAVOURITE_FAILURE";
@@ -12,5 +13,24 @@ export const catFavouriteSuccess = (id) => ({
 });
 export const catFavouriteFailure = (error) => ({
   type: CAT_FAVOURITE_FAILURE,
+  payload: { error },
+});
+
+// DELETE
+
+export const DELETE_CAT_FAVOURITE = "DELETE_CAT_FAVOURITE";
+export const DELETE_CAT_FAVOURITE_SUCCESS = "DELETE_CAT_FAVOURITE_SUCCESS";
+export const DELETE_CAT_FAVOURITE_FAILURE = "DELETE_CAT_FAVOURITE_FAILURE";
+
+export const deleteCatFavourite = (favouriteId) => ({
+  type: DELETE_CAT_FAVOURITE,
+  payload: { favouriteId },
+});
+export const deleteCatFavouriteSuccess = (id) => ({
+  type: DELETE_CAT_FAVOURITE_SUCCESS,
+  payload: { id },
+});
+export const deleteCatFavouriteFailure = (error) => ({
+  type: DELETE_CAT_FAVOURITE_FAILURE,
   payload: { error },
 });
