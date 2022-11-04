f27fb9b30dff4d1c9e789f66c5fb2423d26d1216
michael bazhanov <michaelbazhanov33@gmail.com>
2022-11-04 16:24:01 +0300

    fix

--- src/components/Heart/index.js
+++ src/components/Heart/index.js
@@ -1,5 +1,5 @@
+import React, { useEffect, useState } from "react";
 import { useHover } from "../../utils/hooks/hover";
-import { useEffect, useState } from "react";
 import HeartFill from "./HeartFill";
 import HeartStroke from "./HeartStroke";
 import classNames from "classnames";
