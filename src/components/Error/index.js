74f65f114327ff054cd06b0f5f5604607e75c2bf
michael bazhanov <michaelbazhanov33@gmail.com>
2022-11-09 02:54:00 +0300

    Modification of the Error component. Implemented the page reload timer

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Error = () => {
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count - 1);
+    }, 1000);
+
+    if (count === 0) {
+      window.location.reload();
+      clearTimeout(timer);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [count]);
+
   return (
     <div className="flex flex-col w-full">
-      <h2 className="text-3xl font-bold mb-5 text-center">Sorry. Kitty's crying. There's been some kind of mistake.</h2>
+      <h2 className="text-3xl font-bold mb-5 text-center">
+        Sorry. Kitty's crying. There's been some kind of mistake.
+      </h2>
       <div className="h-80 w-90 bg-weeping-cat bg-no-repeat bg-contain bg-center"></div>
+      <h2 className="text-3xl font-bold mb-5 text-center">
+        The page will be reloaded in {count}
+      </h2>
     </div>
   );
 };
